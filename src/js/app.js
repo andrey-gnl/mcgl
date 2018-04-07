7cd6f56f77ad7caaa544d01e3e1b4f38b5f9d9c8
andrey gnl <a.gnilitskiy@gmail.com>
2018-04-07 17:26:05 +0300

    fixes done

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,13 @@ import svg4everybody from 'svg4everybody';
 import './components';
 
 svg4everybody();
+
+{
+  const cloneParent = $('.js-copy');
+  const cloneItems = cloneParent.find('> .card').clone();
+  $('.js-show-more-blocks').on('click', (e) => {
+    cloneParent.append(cloneItems.clone());
+
+    e.preventDefault();
+  });
+}
