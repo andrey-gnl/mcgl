621e17103646f5a179bd3a899b0199bc5ddb10cd
andrey gnl <a.gnilitskiy@gmail.com>
2018-04-08 15:52:52 +0300

    018-019 pages added

--- src/js/components/editItems.js
+++ src/js/components/editItems.js
@@ -19,3 +19,40 @@ $('.js-edit-cancel, .js-edit-save').on('click', function(e) {
 
   e.preventDefault();
 });
+
+// js edit inputs
+$('.js-change-input').on('click', function(e) {
+  const target = $(this);
+  const items = target.closest('.js-edit').find('.js-edit-input');
+  const parent = target.closest('.js-change');
+
+  items.removeAttr('readonly');
+  parent.addClass('is-active');
+
+  e.preventDefault();
+});
+
+$('.js-edit-save-input').on('click', function(e) {
+  const target = $(this);
+  const items = target.closest('.js-edit').find('.js-edit-input');
+  const parent = target.closest('.js-change');
+
+  items.attr('readonly', true);
+  parent.removeClass('is-active');
+
+  e.preventDefault();
+});
+
+$('.js-edit-cancel-input').on('click', function(e) {
+  const target = $(this);
+  const allParent = target.closest('.js-edit');
+  const items = allParent.find('.js-edit-input');
+  const form = allParent.find('.js-form-edit');
+  const parent = target.closest('.js-change');
+
+  form.get(0).reset();
+  items.attr('readonly', true);
+  parent.removeClass('is-active');
+
+  e.preventDefault();
+});
