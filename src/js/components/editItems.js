34de3dd53e14d8f7f5c05ed80a36d7f516ab326c
andrey gnl <a.gnilitskiy@gmail.com>
2018-04-08 18:21:29 +0300

    pages done

--- src/js/components/editItems.js
+++ src/js/components/editItems.js
@@ -51,13 +51,14 @@ $('.js-edit-cancel-input').on('click', function(e) {
   const parent = target.closest('.js-change');
 
   form.get(0).reset();
+  form.find('input').change();
   items.attr('readonly', true);
   parent.removeClass('is-active');
 
   e.preventDefault();
 });
 
-// js edit inputs
+// js edit profile
 $('.js-change-edit-profiles').on('click', function(e) {
   const target = $(this);
   const parentAll = target.closest('.js-profiles');
@@ -95,9 +96,51 @@ $('.js-edit-cancel-profiles').on('click', function(e) {
   const parent = target.closest('.js-change');
 
   form.get(0).reset();
+  form.find('input').change();
   inputs.attr('readonly', true);
   profile.removeClass('is-removable');
   parent.removeClass('is-active');
+
+  e.preventDefault();
+});
+
+// js edit pin
+$('.js-change-edit-pin').on('click', function(e) {
+  const target = $(this);
+  const parent = target.closest('.js-change');
+  const parentAll = target.closest('.js-pin');
+  const form = parentAll.find('.js-form-pin-wrapper');
+
+  parent.removeClass('is-active');
+  form.addClass('is-edit');
+  parent.addClass('is-active');
+
+  e.preventDefault();
+});
+
+$('.js-edit-save-pin').on('click', function(e) {
+  const target = $(this);
+  const parent = target.closest('.js-change');
+  const parentAll = target.closest('.js-pin');
+  const form = parentAll.find('.js-form-pin-wrapper');
+
+  parent.removeClass('is-active');
+  form.removeClass('is-edit');
+
+  e.preventDefault();
+});
+
+$('.js-edit-cancel-pin').on('click', function(e) {
+  const target = $(this);
+  const parent = target.closest('.js-change');
+  const parentAll = target.closest('.js-pin');
+  const formWrapper = parentAll.find('.js-form-pin-wrapper');
+  const form = parentAll.find('.js-form-pin');
+
+  parent.removeClass('is-active');
+  form.get(0).reset();
+  form.find('input').change();
+  formWrapper.removeClass('is-edit');
 
   e.preventDefault();
 });
